f0cb4ba25d865648cde1f15afa473dde7b911eb9
星 <i@star2000.work>
2019-09-05 11:45:24 +0800

    更新

--- install.ps1
+++ install.ps1
@@ -1,11 +1,12 @@
 $aria2 = "$HOME\.config\aria2"
-$arch = if ([IntPtr]::Size -eq 8) { '64' } else { '32' }
+$auto = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\aria2.vbs"
 
 Get-Process aria2c -ErrorAction Ignore | Stop-Process
-Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\aria2.vbs" -Force | Out-Null
+Remove-Item $auto -Force | Out-Null
 Remove-Item $aria2 -Recurse -Force | Out-Null
 New-Item $aria2 -ItemType Directory | Out-Null
 
+$arch = if ([IntPtr]::Size -eq 8) { 64 } else { 32 }
 $url = Invoke-WebRequest api.github.com/repos/aria2/aria2/releases/latest -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty assets | Where-Object { $_.name -like "*win-$arch*" } | Select-Object -ExpandProperty browser_download_url
 Invoke-WebRequest $url -UseBasicParsing -OutFile "$env:TMP\aria2.zip"
 Expand-Archive "$env:TMP\aria2.zip" "$env:TMP\aria2"
@@ -33,5 +34,5 @@ save-session-interval=60
 "@ | Out-Null
 New-Item "$aria2\aria2.session" | Out-Null
 
-New-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\aria2.vbs" -Value "CreateObject(`"WScript.Shell`").Run `"$aria2\aria2c.exe`",0" | Out-Null
-cscript.exe "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\aria2.vbs" | Out-Null
+New-Item $auto -Value "CreateObject(`"WScript.Shell`").Run `"$aria2\aria2c.exe`",0" | Out-Null
+cscript.exe $auto | Out-Null
