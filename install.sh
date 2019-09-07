3404782fac088663c650c5b43f3487a84643174f
星 <i@star2000.work>
2019-09-07 19:46:38 +0800

    不重复安装

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ mkdir $aria2
 mkdir "$HOME/.local/applications" 2>/dev/null
 mkdir "$HOME/.config/autostart" 2>/dev/null
 
-if which apt >/dev/null; then
-    sudo apt install -y aria2
-elif which yum >/dev/null; then
-    sudo yum install -y aria2
+if ! which aria2c >/dev/null; then
+    if which apt >/dev/null; then
+        sudo apt install -y aria2
+    elif which yum >/dev/null; then
+        sudo yum install -y aria2
+    fi
 fi
 
 echo "
